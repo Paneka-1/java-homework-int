cc836bf0a753a6bd555ddf16aa8fd510effa8032
Katerina <panekkka@mail.ru>
2020-07-21 19:34:48 +0300

    Bank.java added

---
+++ Bank.java
@@ -0,0 +1,8 @@
+public class Bank {
+    public static void main(String[] args) {
+        int client = 2_000_000_000;
+        int money = 500_000_000;
+        int total = client + money;
+        System.out.println(total);
+    }
+}
